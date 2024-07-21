13cd4b989c61c173eb603a77b72fe177ac9aaf72
Abhimanue <abhimanue80@gmail.com>
2024-07-21 14:54:41 +0530

    added create new job fn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ import "../styles/Home.css";
 import Axios from "axios";
 import { FaEdit, FaChevronDown } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { IoAddSharp } from "react-icons/io5";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Input, TextInput, Button } from "@mantine/core";
+import { Modal, Input, TextInput, Button, Text } from "@mantine/core";
 
 const Home = () => {
   const [jobs, setJobs] = useState([{}]); // store all jobs received from back-end
@@ -18,17 +19,25 @@ const Home = () => {
   });
   const [updateSuccess, setUpdateSuccess] = useState(false);
 
+
   const [opened, { open, close }] = useDisclosure(false); // modal open() and close() functions
 
+  const [openedCreate,setOpenedCreate] = useState(false);
+  const [createErrorCompany,setCreateErrorCompany] = useState("");
+  const [createErrorPosition, setCreateErrorPosition] = useState("");
+
+
+
   const handleEdit = (id) => {
-    // when edit button is clicked
-    open(); // open modal
     setFormData({
       // clear the formData from previous use
       company: "",
       position: "",
       status: "",
     });
+    // when edit button is clicked
+    open(); // open modal
+    
     const job = jobs.find((job) => {
       // find the job to be edited using its id
       return job._id === id;
@@ -47,7 +56,7 @@ const Home = () => {
     //       }
     //     );
     //     if (response.status === 200) {
-    //         fetchdata();
+            
     //     }
     //   } 
     // } catch (err) {}
@@ -69,6 +78,70 @@ const Home = () => {
       status: e.target.value,
     });
   };
+
+  const handleChangeCreate = (e)=>{
+     const { name, value } = e.target;
+     setFormData({
+       ...formData,
+       [name]: value,
+     });
+  }
+  const handleSelectChangeCreate = (e)=>{
+     setFormData({
+       //available dynamicaly
+       ...formData,
+       status: e.target.value,
+     });
+  }
+  const handleSubmitCreate = async (e)=>{
+    e.preventDefault();
+    if (formData.status === "") {
+      formData.status = "pending";
+    }
+    setCreateErrorCompany("");
+    setCreateErrorPosition("");
+    try {
+      const token = localStorage.getItem("jwtToken");
+      if (token) {
+        const response = await Axios.post(
+          `https://jobs-api-0v7l.onrender.com/api/v1/jobs/`,
+          formData,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        if (response.status === 201) {
+          setFormData({
+            // clear the formData from previous use
+            company: "",
+            position: "",
+            status: "",
+          });
+            // fetchdata();
+          setUpdateSuccess(true);
+          setTimeout(() => {
+            setUpdateSuccess(false);
+            setOpenedCreate(false);
+          }, 1000);
+        }
+      } else {
+        //redirect to session expired please login again page
+      }
+    } catch (err) {
+      // deal with error
+      let errTexts  = err.response.data.msg;
+      errTexts = errTexts.split(',');
+      for(let i=0;i<errTexts.length;i++){
+        const errText = errTexts[i];
+        if (errText.includes("company")) {
+          setCreateErrorCompany(errText)
+        }
+        if (errText.includes("position")) {
+          setCreateErrorPosition(errText)
+        }
+      }
+    }
+  }
 
   const handleSubmit = async (e) => {
     // update the job
@@ -93,6 +166,12 @@ const Home = () => {
           }
         );
         if (response.status === 200) {
+          setFormData({
+            // clear the formData from previous use
+            company: "",
+            position: "",
+            status: "",
+          });
             // fetchdata();
           setUpdateSuccess(true);
           setTimeout(() => {
@@ -131,6 +210,27 @@ const Home = () => {
   return (
     <div>
       <Header />
+      <div className="create-new">
+        <Text variant="gradient" fw={900} size="lg">
+          Create new job
+        </Text>
+        <div
+          onClick={() => {
+            setFormData({
+              // clear the formData from previous use
+              company: "",
+              position: "",
+              status: "",
+            });
+            setCreateErrorCompany("");
+            setCreateErrorPosition("");
+            setOpenedCreate(true);
+          }}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <IoAddSharp fontSize={20} color="grey" />
+        </div>
+      </div>
       <div className="jobs-wrapper">
         {jobs.map((job, key) => {
           return (
@@ -167,7 +267,13 @@ const Home = () => {
           );
         })}
       </div>
-      <Modal opened={opened} onClose={close} title="Edit Job" radius= "md" centered>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Edit Job"
+        radius="md"
+        centered
+      >
         <form>
           <TextInput
             label="Company"
@@ -207,7 +313,54 @@ const Home = () => {
         </form>
       </Modal>
 
-      {/* <Button onClick={open}>Open centered Modal</Button> */}
+      <Modal
+        opened={openedCreate}
+        onClose={() => {
+          setOpenedCreate(false);
+        }}
+        title="Edit Job"
+        radius="md"
+        centered
+      >
+        <form>
+          <TextInput
+            label="Company"
+            placeholder="Company name"
+            onChange={handleChangeCreate}
+            name="company"
+            // error={createErrors.company}
+            error={createErrorCompany}
+          ></TextInput>
+          <TextInput
+            label="Position"
+            placeholder="Position"
+            onChange={handleChangeCreate}
+            name="position"
+            // error={createErrors.position}
+            error={createErrorPosition}
+          ></TextInput>
+          <Input
+            component="select"
+            rightSection={<FaChevronDown size={14} stroke={1.5} />}
+            pointer
+            mt="md"
+            onChange={handleSelectChangeCreate}
+          >
+            <option value="pending">Pending</option>
+            <option value="interview">Interview</option>
+            <option value="declined">Declined</option>
+          </Input>
+          <Button
+            type="submit"
+            fullWidth
+            style={{ marginTop: "15px" }}
+            onClick={handleSubmitCreate}
+            color={updateSuccess ? "teal" : "blue"}
+          >
+            {updateSuccess ? "Task created" : "Confirm"}
+          </Button>
+        </form>
+      </Modal>
     </div>
   );
 };
